ae8643105073a494d04af0eb37e028d9de8194d5
Yohannah <yohannahlv@gmail.com>
2024-10-29 10:24:25 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('theme-toggle').addEventListener('click', function () {
+    const currentTheme = document.documentElement.getAttribute('data-bs-theme');
+    const newTheme = currentTheme === 'light' ? 'dark' : 'light';
+    document.documentElement.setAttribute('data-bs-theme', newTheme);
+    localStorage.setItem('theme', newTheme);
+});
